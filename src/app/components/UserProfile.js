9528ca5c51b4ea09de40657ee6a859a6f121dd5d
Firdevs <firdes.sahin92@gmail.com>
2025-02-04 22:44:21 +0300

    feat: Add removeUser functionality to UserProfile component

--- src/app/components/UserProfile.js
+++ src/app/components/UserProfile.js
@@ -1,17 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-export default function UserProfile({ user, posts }) {
+export default function UserProfile({ user, posts,removeUser }) {
   const userPost = posts.find((post) => post.userId === user.id) || null;
   return (
     <div className=" p-5 border border-gray-200 bg-yellow-100 rounded-md w-1/2 ">
       <div className="flex flex-col items-start gap-2">
-        <div className="flex items-center gap-2">
-          <span className="font-bold">
-            {user.name} {user.username}
-          </span>
+        <div className="flex flex-row w-full items-center justify-between ">
+          <div className="flex flex-col items-start  gap-2">
+            <span className="font-bold">
+              {user.name} {user.username}
+            </span>
+            <p className="text-sm">
+              <b>Email:</b>
+              {user.email}
+            </p>
+          </div>
+          <button onClick={()=>removeUser(user.id)} className="bg-orange-700 text-white px-3">Delete</button>
         </div>
-        <p className="text-sm">{user.email}</p>
         <div>
           {userPost ? (
             <div
