16d8d05fe6981832d72e843a6cb5f52155a7cb40
Firdevs <firdes.sahin92@gmail.com>
2025-02-04 22:44:52 +0300

    feat: Pass removeUser function to UserProfile in UsersList component

--- src/app/components/UsersList.js
+++ src/app/components/UsersList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import UserProfile from "./UserProfile";
 
-export default function UsersList({ users, posts }) {
+export default function UsersList({ users, posts,removeUser }) {
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Kullanıcı Listesi</h2>
       {users.map((user) => {
-        return <UserProfile key={user.id} user={user} posts={posts} />;
+        return <UserProfile key={user.id} user={user} posts={posts} removeUser={removeUser}/>;
       })}
     </div>
   );
